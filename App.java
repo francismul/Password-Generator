e3530a722d39aa1f6ffa6a2490decbc08e6605ba
Francis Mule <francismule300@gmail.com>
2025-03-19 00:22:50 +0300

    Formatted the code to look appealing

--- App.java
+++ App.java
@@ -37,13 +37,10 @@ class App {
     }
 
     public static List<String> generateRandomPassword(Scanner scanner) {
-        List<String> lowercase = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-        List<String> uppercase = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-                "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-        List<String> digits = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-        List<String> punctuation = Arrays.asList("!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+",
-                ",", "-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
+        List<String> lowercase = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
+        List<String> uppercase = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"));
+        List<String> digits = new ArrayList<>(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"));
+        List<String> punctuation = new ArrayList<>(Arrays.asList("!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"));
 
         System.out.print("Enter the number of characters: ");
         int characterNumber;
